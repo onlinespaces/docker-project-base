dc4f3f00742df9c1b00fbe58daf5b409b11e3839
Scott Collier <scott.collier@umuc.edu>
2016-12-22 13:55:00 +0100

    Check for application type and use the appropriate conf file

--- docker/nginx/docker-entrypoint.sh
+++ docker/nginx/docker-entrypoint.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+if [ ! -d "/etc/nginx/sites-enabled" ]; then
+    mkdir /etc/nginx/sites-enabled
+fi
+
+if [ -n "$APPLICATION_TYPE" ]; then
+    if [ "$APPLICATION_TYPE" = "drupal8" ]; then
+        cp /etc/opt/docker-files/drupal8.conf /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
+    else
+        cp /etc/opt/docker-files/symfony.conf /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
+    fi
+fi
+
 sed -i "s/#DOMAIN_NAME#/${DOMAIN_NAME}/g" /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
 sed -i "s/#PROJECT_NAME#/${PROJECT_NAME}/g" /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
 sed -i "s/#PHP_FPM_PORT#/${PHP_FPM_PORT}/g" /etc/nginx/sites-enabled/${PROJECT_NAME}.conf
